0ce3873d71bf854296850d1fc80d190b259348ea
Yurizinho800 <157324630+yurizinho800@users.noreply.github.com>
2024-09-16 00:32:59 -0300

    Update fundamento.py

--- test/fundamento.py
+++ test/fundamento.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     tecnologias_encontradas = obter_tecnologias(site_digitado)
 
     if tecnologias_encontradas:
-        print(Fore.GREEN + "Tecnologias encontradas no site:")
+        print(Fore.YELLOW + "Tecnologias encontradas no site:")
         for tecnologia in tecnologias_encontradas:
             print(tecnologia)
     else:
