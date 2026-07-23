741e7f37781286b60481fd6875bab4ec99166dc1
dsnoedi31 <yuridotaumodficdeferro@gmail.com>
2026-07-23 10:37:22 -0300

    Conquistando o badge Pair Extraordinaire
    Co-authored-by: dsnoedi31 <yuridotaumodficdeferro@gmail.com>

--- test/extras/como.py
+++ test/extras/como.py
@@ -7,15 +7,17 @@ def abrir_menu():
             "ip.py",
             "número-celular",
             "tecnologia",
-            "protocolo",
+            "ipv6",
             "info",
-            "whois",
-            "colaboradoes"
+            "portas",
+            "pesquisa",
+            "dados",
+            "sql"
         ]
         for i, opcao in enumerate(opcoes, start=1):
             print(f"{i}. {opcao}")
 
-        selecao = int(input("\nEscolha uma opção (1 a 7): "))
+        selecao = int(input("\nEscolha uma opção (1 a 9): "))
 
         if selecao == 1:
             print(Fore.BLUE + "Basta inserir o domínio do site. Exemplo: www.site.com.br")
@@ -24,19 +26,23 @@ def abrir_menu():
         elif selecao == 3:
             print(Fore.YELLOW + "Coloque o domínio do site com http ou https.")
         elif selecao == 4:
-            print(Fore.BLUE + "Não é necessário incluir http ou https. Exemplo: www.site.com.")
+            print(Fore.BLUE + "ainda em desenvolvimento.")
         elif selecao == 5:
             print(Fore.BLUE + "Você pode usar o Osint para pesquisar informações como e-mails ou sites semelhantes.se deseja procurar por links, pode começar pelo ´www....com´.  já o personalizado precisa de http ou https.")
         elif selecao == 6:
-            print(Fore.BLUE + "Para usar o whois, insira o domínio. Exemplo: www.site.com.br.")
+            print(Fore.BLUE + "não precisa usar protocolos, apenas o domínio")
         elif selecao == 7:
-            print(Fore.BLUE + "participem do meu grupo do facebook")
+            print(Fore.BLUE + "por enquanto essa opção só pode ser usada no linux.")
+        elif selecao == 8:
+            print(Fore.BLUE + "Para usar o whois, insira o domínio. Exemplo: www.site.com.br.")
+        elif selecao == 9:
+            print(Fore.BLUE + "usa-se protocolo http ou https.")
         else:
-            print(Fore.RED + "Opção inválida. Escolha um número de 1 a 7.")
+            print(Fore.RED + "Opção inválida. Escolha um número de 1 a 9.")
 
     except ValueError:
         print(Fore.RED + "Erro: insira um número válido.")
 
-# Exemplo de uso:
+
 abrir_menu()
 
